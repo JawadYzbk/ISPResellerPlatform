f75494e6a54c73f555c55fec863b2381cc110c57
JawadYzbk <jawadyazbek@gmail.com>
2026-08-11 18:07:53 +0300

    test: isolate docker database from feature tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,6 +7,26 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    public function createApplication()
+    {
+        foreach ([
+            'APP_ENV' => 'testing',
+            'CACHE_STORE' => 'array',
+            'DB_CONNECTION' => 'sqlite',
+            'DB_DATABASE' => ':memory:',
+            'DB_URL' => '',
+            'MAIL_MAILER' => 'array',
+            'QUEUE_CONNECTION' => 'sync',
+            'SESSION_DRIVER' => 'array',
+        ] as $key => $value) {
+            putenv($key.'='.$value);
+            $_ENV[$key] = $value;
+            $_SERVER[$key] = $value;
+        }
+
+        return parent::createApplication();
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
