41fd834617bce16f33ed186c97ea991a52370f3e
JawadYzbk <jawadyazbek@gmail.com>
2026-08-11 18:07:53 +0300

    fix: keep development configuration uncached

--- docker/php/start-development.sh
+++ docker/php/start-development.sh
@@ -9,7 +9,7 @@ if [ ! -f vendor/autoload.php ] || [ composer.json -nt vendor/autoload.php ] || 
 fi
 
 php artisan migrate --force --no-interaction
-php artisan config:cache --no-interaction
+php artisan config:clear --no-interaction
 
 cd public
 exec php -d opcache.validate_timestamps="${PHP_OPCACHE_VALIDATE_TIMESTAMPS:-0}" -S 0.0.0.0:8000 ../vendor/laravel/framework/src/Illuminate/Foundation/resources/server.php
